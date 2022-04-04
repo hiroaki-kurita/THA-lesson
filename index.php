65e5e7db7dd522323e3b17a3b0820594a5453e1e
hiroaki-kurita <hakurou4330@gamil.com>
2022-04-04 21:27:42 +0900

    画面作成

---
+++ index.php
@@ -0,0 +1,371 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <title>Document</title>
+    <!-- スタイルシート -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="block-header">
+        <img src="img/logo.png" alt="THAロゴ">
+        <ul class="header-list">
+            <li class="lesson">
+                <a href="#lesson-detail">認定講座</a>
+            </li>
+            <li class="about">
+                <a href="#">協会について</a>
+            </li>
+            <li class="overview">
+                <a href="#">協会概要</a>
+            </li>
+        </ul>
+    </div>
+    <div class="block-main">
+        <div class="main-title">
+            <h2>トータルヘルスケアで、仕事も、健康も、グレードアップ</h2>
+            <h1>健康も、幸せも、私はここで手に入れた</h1>
+        </div>
+        <div id="lesson-detail">
+            <h3>認定講座について</h3>
+            <h4>大切な自分と家族の為に、今、健康についてできること</h4>
+            <div class="detail-content">
+                <div class="detail-image">
+                    <img src="img/familycooking.png" alt="家族団欒の食卓">
+                </div>
+                <div class="detail-text">
+                    <p>母として、女性として、いつだって気になってくるのは</p>
+                    <p>「健康」のこと。女性としては、体型も気になるという方</p>
+                    <p>もいらっしゃるのではないでしょうか。</p>
+                    <p><br></p>
+                    <p>お子様には健やかに育って欲しいと願い、お子様の自立</p>
+                    <p>後は自分と旦さまの健康が気になりだします。</p>
+                    <p><br></p>
+                    <p>これだけ生活に根付いた健康を、最も手軽に、日常的に取り</p>
+                    <p>入れられるのが「食事」です。</p>
+                    <p><br></p>
+                    <p>トータルヘルスケア協会では、美と健康に繋がるおいしいレ</p>
+                    <p>ッスンを、<span>万全サポートで学ぶことができます。</span></p>
+                    <p><br></p>
+                    <p>家族全員の健康の為に、まずは一歩踏み出してみませんか？</p>
+                </div>
+            </div>
+        </div>
+        <div class="customer-voice">
+            <div class="voice-title">
+                <h4>受講された皆様から</h4>
+                <h4>たくさんのお声をいただいております！</h4>
+            </div>
+            <div class="voice-first">
+                <p>味噌汁の品目を増やすなど今の食生活に少し工夫するだけで改善することができ、育児中で時間のない</p>
+                <p>中でも実践しやすかったのと、<span>先生からアドバイスやフィードバックを頂くことで毎日続ける活力</span>にな</p>
+                <p>りました。</p>
+                <div class="first-name">
+                    <p>N.K.様</p>
+                </div>
+            </div>
+            <div class="voice-second">
+                <p>先生から頂いたアドバイスを実践することで<span>食事の幅が広がりました。</span></p>
+                <p>何より先生に質問しやすい環境がとても助かりました。</p>
+                <p>私だけでなく、家族の食事の悩みにもアドバイスを下さり、子どもと一緒にご飯を作る機会が増えて家</p>
+                <p>族で美味しく食べることができました。</p>
+                <div class="second-name">
+                    <p>M.Y.様</p>
+                </div>
+            </div>
+            <div class="voice-third">
+                <p>生活習慣の改善は、正しさだけでなく、<span>いかに自分らしく豊かな生活を送れるかだと思う。</span></p>
+                <p>10年後、20年後といった先の暮らしを描きながら、今一度振り返るよい機会になった。</p>
+                <div class="third-name">
+                    <p>T.H.様</p>
+                </div>
+            </div>
+        </div>
+        <div class="license-step">
+            <h4>資格取得で描くステップアップ</h4>
+            <div class="step-text">
+                <p>トータルヘルスケア協会では、下記のような資格取得が可能です。とにかく実践的にヘルシーなお料理を学びたい方か</p>
+                <p>ら、キャリアまでしっかり描けるものまで、各種取り揃えております。</p>
+            </div>
+            <img src="img/stepup.png" alt="ステップアップイメージ図">
+        </div>
+        <div class="basic-lesson">
+            <div class="b-lesson-title">
+                <h3>基礎講座</h3>
+                <h4>美と健康を確実に手に入れるサポートを。</h4>
+            </div>
+            <div class="b-lesson-detail">
+                <div class="b-lesson-image">
+                    <img src="img/lesson-image.png" alt="レッスンのイメージ図">
+                </div>
+                <div class="b-lesson-text">
+                    <p>いつも美味しい食事を家族に振る舞うみなさんだと思います</p>
+                    <p>が、日々ついてまわるのが「今晩の献立」。できれば、家族</p>
+                    <p>が健康的でいられるものは何かを常に考えていらっしゃるの</p>
+                    <p>ではないかなと思います。</p>
+                    <p><br></p>
+                    <p>トータルヘルスケア協会が提供する基礎講座は、バランスの</p>
+                    <p>取れた食事レパートリーを増やし、確実に健康へと導いてい</p>
+                    <p>きます。</p>
+                    <p><br></p>
+                    <p>知識と実践で、より魅力ある生活に彩りを加えてみませんか？</p>
+                </div>
+            </div>
+            <div class="basic-detail">
+                <div class="basic-title">
+                    <img src="img/trial.png" alt="トライアル">
+                    <h3>ベーシックコース</h3>
+                </div>
+                <div class="basic-content">
+                    <div class="basic-image">
+                        <img src="img/basic-image.png" alt="ベーシックコースのイメージ図">
+                    </div>
+                    <div class="basic-text">
+                        <span>コース説明</span>
+                        <p>バランスの取れた料理のレパートリーを増やし、美味しく食べて家族も</p>
+                        <p>皆、健康になります。</p>
+                        <p><br></p>
+                        <p><br></p>
+                        <span>受講内容</span>
+                        <p>1.オンラインレッスン　6回</p>
+                        <p>2.Line及びZOOMサポート（30分×6回）</p>
+                    </div>
+                </div>
+            </div>
+            <div class="support-detail">
+                <h3>サポートコース</h3>
+                <div class="support-content">
+                    <div class="support-image">
+                        <img src="img/support-image.png" alt="サポートコースのイメージ図">
+                    </div>
+                    <div class="support-text">
+                        <span>コース説明</span>
+                        <p>バランスの取れた料理のレパートリーを増やし、美味しく食べて家族も</p>
+                        <p>皆、健康になります。</p>
+                        <p>さらにベーシックコースに加え、オリジナルレシピなど、嬉しいサポー</p>
+                        <p>トが追加されたコースです。</p>
+                        <span>受講内容</span>
+                        <p>1.オンラインレッスン　6回</p>
+                        <p>2.Line及びZOOMサポート（30分×6回）</p>
+                        <p>3.あなたにカスタマイズしたオリジナルレシピと作り⽅動画</p>
+                        <p>　（料理によってはレシピのみ）・24 レシピ</p>
+                    </div>
+                </div>
+            </div>
+            <div class="fullsupport-detail">
+                <div class="fullsupport-title">
+                    <img src="img/popular.png" alt="ポピュラー">
+                    <h3>フルサポートコース</h3>
+                </div>
+                <div class="fullsupport-content">
+                    <div class="fullsupport-image">
+                        <img src="img/fullsupport-image.png" alt="ベーシックコースのイメージ図">
+                    </div>
+                    <div class="fullsupport-text">
+                        <span>コース説明</span>
+                        <p>バランスの取れた料理のレパートリーを増やすだけでなく、知識を得る</p>
+                        <p>ことで食事で健康を維持、コントロールできるようになります。</p>
+                        <span>資格取得</span>
+                        <p>トータルヘルスケアフードアドバイザー3級取得が可能です。</p>
+                        <span>受講内容</span>
+                        <p>1.オンラインレッスン　6回</p>
+                        <p>2.Line及びZOOMサポート（30分×6回）</p>
+                        <p>3.あなたにカスタマイズしたオリジナルレシピと作り⽅動画</p>
+                        <p>　（料理によってはレシピのみ）・24 レシピ</p>
+                        <p>4.毎日の食事記録 アドバイス</p>
+                        <p>5.栄養についての講義（90分×6回）</p>
+                        <div class="nutrition-lesson">
+                            <p>①　身体のしくみ、免疫について</p>
+                            <p>②　身体に必要な栄養について</p>
+                            <p>③　正しい食生活の為の基本的な知識</p>
+                            <p>④　病気にならない為の食習慣と生活習慣</p>
+                            <p>⑤　栄養についての知識 1</p>
+                            <p>⑥　栄養についての知識 2</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="inquiry-button">
+                <img src="img/speech-bubble.png" alt="吹き出し">
+                <a href="#form"><img src="img/inquiry-button.png" alt="お問い合わせボタン"></a>
+            </div>
+        </div>
+        <div class="t-training-lesson">
+            <div class="t-lesson-title">
+                <h3>育成講座</h3>
+                <h4>ご自分の家族だけではなく、仕事に繋げて、さらに輝く人生へ</h4>
+            </div>
+            <div class="t-lesson-detail">
+                <div class="t-lesson-image">
+                    <img src="img/lesson-image2.png" alt="レッスンのイメージ図2">
+                </div>
+                <div class="t-lesson-text">
+                    <p>さらに知識を深めることで、家族だけではなく仕事へも活か</p>
+                    <p>すことができます。</p>
+                    <p>基礎講座でつけた知識や食事レパートリーを活用して、自分</p>
+                    <p>自身の人生をもっとハリのある豊かなものへ磨き上げていき</p>
+                    <p>ましょう。</p>
+                    <p><br></p>
+                    <p>トータルヘルスケア協会では、そのためのサポート体制を万</p>
+                    <p>全に、日常だけではない深い学びをご提供致します。</p>
+                    <p>きます。</p>
+                </div>
+            </div>
+            <div class="adviser2-detail">
+                <h3>トータルヘルスケアフードアドバイザー２級　講習</h3>
+                <div class="adviser2-content">
+                    <div class="adviser2-image">
+                        <img src="img/adviser2.png" alt="アドバイザー２級">
+                    </div>
+                    <div class="adviser2-text">
+                        <span>コース説明</span>
+                        <p>知識を生かしながら実際にアドバイスできるスキルを身に付られます。</p>
+                        <p>資格試験合格後は 『トータルヘルスケアフードアドバイザー』として 食</p>
+                        <p>事指導やサポートが出来ます。</p>
+                        <p><br></p>
+                        <p><br></p>
+                        <span>受講内容</span>
+                        <p>1.個別サポート２名</p>
+                        <div class="adviser2-memo">
+                            <p>　自身が集客したモニター２名を、講師のアドバイスを受けながらその方に合ったカス</p>
+                            <p>　タマイズメニューを選択し、食事記録のアドバイスや 指導、サポートを行う。</p>
+                        </div>
+                        <p>2.パワーポイントを使ってのデモレッスン（グループレッスン）6回</p>
+                        <p>3.臨床栄養学についての講義（90分×3回）</p>
+                        <div class="adviser2-memo">
+                            <p>　①糖尿病（ダイエット）についての知識と必要な栄養素</p>
+                            <p>　②高血圧についての知識と必要な栄養素</p>
+                            <p>　③不妊についての知識と必要な栄養素</p>
+                        </div>
+                        <p>4.健康を維持する為に、必要な食事以外の分野の専門家による講義</p>
+                        <div class="adviser2-memo">
+                            <p>　90分×3回ー動画レッスン</p>
+                            <p>　①睡眠　②メンタル　③運動</p>
+                        </div>
+                        <p>5.集客の為の講習</p>
+                        <p>6.レシピ・動画 自由に閲覧可能</p>
+                        <div class="adviser2-memo">
+                            <p>　1500以上あるレシピ・動画を自由に閲覧可能。</p>
+                            <p>　（自身の家庭での食事、またサポート目的での使用に限る）</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="adviser1-detail">
+                <h3>その後、1級（講師）、シニア（認定講師）へ ステップアップ</h3>
+                <div class="adviser1-content">
+                    <div class="adviser1-image">
+                        <img src="img/adviser1.png" alt="アドバイザー１級">
+                    </div>
+                    <div class="adviser1-text">
+                        <p>トータルヘルスケアフードアドバイザー2級取得後は、さらに順を追っ</p>
+                        <p>て、トータルヘルスケアフードアドバイザー1級、シニアトータルヘル</p>
+                        <p>スケアフードアドバイザーへとステップアップすることができます。</p>
+                        <p><br></p>
+                        <p>これにより様々な物事に生かすことができ、社会的にもより充実した</p>
+                        <p>毎日を送ることが可能になります。</p>
+                    </div>
+                </div>
+            </div>
+            <a href="#form"><img src="img/inquiry-button.png" alt="お問い合わせボタン"></a>
+        </div>
+        <div class="price">
+            <h3>料金表</h3>
+            <img src="img/price.png" alt="コース料金表">
+            <div class="inquiry-button">
+                <img src="img/speech-bubble.png" alt="吹き出し">
+                <a href="#form"><img src="img/inquiry-button.png" alt="お問い合わせボタン"></a>
+            </div>
+        </div>
+        <div class="association-about">
+            <h3>当協会について</h3>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/miHgdyyHj1M" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <p>身体は食べたものでしか作られない、という事で 　現在のその方の不調や健康状態に合わせた 必要な知識や、個別にカ</p>
+            <p>スタマイズした料理メニューをご提案。</p>
+            <p>レシピ提案だけでなく、作り方講習や詳しく解説した動画を提供する事で、 健康的な食習慣が作れるようにする。</p>
+            <p>そうする事で、健康を維持し、将来の不安をなくして 心身共に健康な身体作りをめざす。</p>
+        </div>
+        <div class="association-overview">
+            <h3>協会概要</h3>
+            <h4>経営理念</h4>
+            <p>テキスト。テキスト。テキスト。テキスト。テキスト。テキス</p>
+            <p>テキスト。テキスト。テキスト。テキスト。テキスト。テキス</p>
+            <p>テキスト。テキスト。テキスト。テキスト。テキスト。テキス</p>
+            <p>テキスト。テキスト。テキスト。テキスト。テキスト。テキス</p>
+            <img src="img/toshiko.png" alt="栗田登志子のサイン">
+            <img src="img/toshiko2.png" alt="栗田登志子の写真">
+        </div>
+        <div id="mail-form">
+            <h3>お問合わせ</h3>
+            <p>講座お申し込みをご希望の方は、以下のお申込フォームに必要事項をご入力下さい。</p>
+            <p>３営業日以内に●メールアドレス●よりご連絡させて頂きます。</p> 
+            <p><br></p>
+            <p>内容に間違いがなければ次の確認画面で【送信】ボタンを押してください。</p>
+            <div class="form-text">
+                <form action="confirm.php" method="post">
+                    <div class="name">
+                        <label>ご氏名</label>
+                        <input type="text" name="name" value="">
+                    </div>
+                    <div class="phone">
+                        <label>お電話番号</label>
+                        <input type="text" name="phone" value="">
+                    </div>
+                    <div class="email">
+                        <label>メールアドレス</label>
+                        <input type="text" name="email" value="">
+                    </div>
+                    <div class="sex">
+                        <input type="radio" name="woman" value="" checked >女性
+                        <input type="radio" name="man" value="" >男性
+                        <input type="radio" name="other" value="" >その他
+                    </div>
+                    <div class="age">
+                        <input type="radio" name="recruitment" value="" checked >10代
+                        <input type="radio" name="recruitment" value="" >20代
+                        <input type="radio" name="recruitment" value="" >30代
+                        <input type="radio" name="recruitment" value="" >40代
+                        <input type="radio" name="recruitment" value="" >50代
+                        <input type="radio" name="recruitment" value="" >60代以上
+                    </div>
+                    <div class="address">
+                        <label>お住まいの都道府県</label>
+                        <input type="text" name="address" value="">
+                    </div>
+                    <div class="reserve">
+                        <label>第１希望</label>
+                        <input type="text" name="reserve1" value="">
+                        <label>第２希望</label>
+                        <input type="text" name="reserve2" value="">
+                        <label>第３希望</label>
+                        <input type="text" name="reserve3" value="">
+                    </div>
+                    <div class="memo">
+                        <label>ご質問がございましたら、ご自由にご記載ください。</label>
+                        <input type="text" name="memo" value="">
+                    </div>
+                    <div class="mail-policy">
+                        <p>Docomo・au・softbankのキャリアメールやicloud.comのメールアドレスは、こちらからのメールが届きにくい場合があります。</p>
+                        <p>yahoo.co.jp（yahooメール）、gmail.com（Gmail）でのご登録を推奨しています。ご登録いただいたメールアドレスは弊社プライバ</p>
+                        <p>シーポリシーに基づき厳重に管理します。また、ご登録いただいたメールアドレスに無料のメールマガジンをお送りしています。登録</p>
+                        <p>解除をご希望の際にはメール内の解除リンクから簡単に登録解除できます。また、メールマガジンの中で弊社が提供している有料サー</p>
+                        <p>ビスをご案内することがございますが、無料のメールマガジンから自動的に有料サービスに登録されるようなことは一切ありませんの</p>
+                        <p>で、ご安心ください。</p>
+                    </div>
+                    <button type="submit">送信</button>
+                </form>
+            </div>
+        </div>
+        <div class="privacy-policy">
+            <a href="#">プライバシー</a>
+            <a href="#">特定商取引法に基づく表示</a>
+        </div>
+    </div>
+    <div class="block-footer">
+        <p>Copyright ©︎ トータルヘルスケア協会</p>
+    </div>
+</body>
+</html>
