858fd8b0a9b7744632f96ec459f8e6be15a7b9f6
hiroaki-kurita <hakurou4330@gamil.com>
2022-04-16 18:53:14 +0900

    js消去

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- スタイルシート -->
     <link rel="stylesheet" href="style.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+    
 </head>
 <body>
     <div class="container">
@@ -466,6 +466,6 @@
         </div>
     </div>
     <!-- js -->
-    <script src="form.js"></script>
+
 </body>
 </html>
